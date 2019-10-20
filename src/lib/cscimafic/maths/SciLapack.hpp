b2d48586058976b411023f8004f193777aa83487
Petr Kulhanek <kulhanek@chemi.muni.cz>
2019-10-20 09:12:06 +0200

    mkl threading setup

--- src/lib/cscimafic/maths/SciLapack.hpp
+++ src/lib/cscimafic/maths/SciLapack.hpp
@@ -33,6 +33,9 @@ public:
     /// print info about Blas/Lapack
     static void PrintExecInfo(CVerboseStr& vout);
 
+    /// set number of threads for Lapak/Blas
+    static void SetNumThreadsLocal(int nthrs);
+
     ///  computes all eigenvalues and, optionally, eigenvectors of a real symmetric matrix A
     //  eigenvectors are stored columnwise
     static int syev(char jobz,char uplo,CFortranMatrix& a,CVector& w,CVector& work);
